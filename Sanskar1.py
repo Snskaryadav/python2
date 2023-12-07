83414f98dda99143f6e68ebd16dce660f7916419
Sanskar Yadav <152489259+Snskaryadav@users.noreply.github.com>
2023-12-07 22:47:31 +0530

    Add files via upload

---
+++ Sanskar1.py
@@ -0,0 +1,7 @@
+def fun1(name = 'Padma', age = 12):
+    '''Body of the function'''
+    print (name, "is", age, "years old.")
+fun1('Aruna',16)
+fun1('Karuna',16) or fun1(age=16, name='Karuna')
+fun1('karuna')
+fun1('padma')
